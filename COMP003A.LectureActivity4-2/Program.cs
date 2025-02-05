c78b4175d3026a3249fb9131e7f71ba23f68048e
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-05 12:32:55 -0800

    Add project files.

---
+++ COMP003A.LectureActivity4-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Pedro Garcia
+// COMP003A
+// Johnathan Cruz
+// Practice implementing methods in C#
+namespace COMP003A.LectureActivity4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
