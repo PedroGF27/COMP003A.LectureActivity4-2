ba989534cabf0fc010cfa77ea4a649272e8a7601
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-05 12:41:14 -0800

    implemented GreetUser, CalculateSum, and DisplayArray methof

--- COMP003A.LectureActivity4-2/Program.cs
+++ COMP003A.LectureActivity4-2/Program.cs
@@ -10,5 +10,35 @@ namespace COMP003A.LectureActivity4_2
         {
             Console.WriteLine("Hello, World!");
         }
+        /// <summary>
+        /// Greets the user by their name
+        /// </summary>
+        static void GreetUser()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine(); // store user input in {name}
+            Console.WriteLine($"Hello, {name}! Welcome to the Methods Activity!");
+        }
+        /// <summary>
+        /// Calculate sum of 2 numbers and returns result
+        /// </summary>
+        /// <param name="num1">Integer 1</param>
+        /// <param name="num2">Integer 2</param>
+        /// <returns></returns>
+        static int CalculateSum(int num1, int num2)
+        {
+            return num1 + num2; // Add 2 numbers and return result
+        }
+        /// <summary>
+        /// Display contents of array.
+        /// </summary>
+        /// <param name="numbers">Array collection of integers</param>
+        static void DisplayArray(int[] numbers)
+        {
+            foreach (int num in numbers) // loop through array
+            {
+                Console.WriteLine(num); // Print each number
+            }
+        }
     }
 }
